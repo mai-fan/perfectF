98923eab4632fc70c1346347fcd0f5b8d90e8018
mai-fan <dectot.gaspard5@gmail.com>
2025-11-29 20:31:27 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let toutesLesDonnees = [];
 
     // --- 1. Récupération des données ---
-    fetch('data/data.json')
+    fetch('https://github.com/mai-fan/perfectF/blob/main/data/data.json')
         .then(response => {
             if (!response.ok) throw new Error(`Erreur HTTP! Statut: ${response.status}`);
             return response.json();
